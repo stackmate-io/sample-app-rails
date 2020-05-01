6627349ae8ac0ac76aab8abf26e50b3c6f33f59f
Fotis Alexandrou <falexandrou@gmail.com>
2020-05-01 21:19:11 +0300

    try to use verified emails

--- app/mailers/sre_mailer.rb
+++ app/mailers/sre_mailer.rb
@@ -2,6 +2,6 @@ class SreMailer < ApplicationMailer
   default from: "system@rails.ezploy.eu"
 
   def incident_report
-    mail(to: 'sre-ezploy-eu@mailinator.com', subject: 'Incident report')
+    mail(to: 'cloudwareventuresdotcom@gmail.com', subject: 'Incident report')
   end
 end
